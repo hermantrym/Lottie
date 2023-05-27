2a0034e9e87f90143d18fd3a78f9c8da3a21a2e2
herman <hermantrym@gmail.com>
2023-05-28 00:54:06 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Lottie - Android Mobile App
+
+### Kotlin
+
+<img src="record.png" alt="Record" width="250" height="420">
---
+++ app/src/main/java/com/ctl/lottie/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.ctl.lottie
+
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.ctl.lottie.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+    }
+}
